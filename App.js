43b94ae8a9cbc94e8c0569fedde7ff3eb8daaa9c
jaxoncreed <jaxoncreed@gmail.com>
2023-07-02 16:43:27 -0400

    solid-authn-react-native works

--- App.js
+++ App.js
@@ -1,20 +1,30 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Button, StyleSheet, Text, View } from 'react-native';
+import { login, getDefaultSession, handleIncomingRedirect } from "solid-authn-react-native";
+import { useEffect, useState } from 'react';
 
 export default function App() {
+  const [session, setSession] = useState({});
+  useEffect(() => {
+    setSession({ ...getDefaultSession().info });
+    handleIncomingRedirect().then(() => {
+      console.log(getDefaultSession().info);
+      setSession({ ...getDefaultSession().info });
+    });
+  }, []);
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View>
+      <Text>Look in the console for more information</Text>
+      <Text>Is Logged In: {session.isLoggedIn}</Text>
+      <Text>WebId: {session.webId}</Text>
+      <Button
+        onPress={() => login({
+          oidcIssuer: "https://solidweb.me/",
+          redirectUrl: window.location.href,
+          clientName: "SomeClient",
+        })}
+        title="Log In"
+      />
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
