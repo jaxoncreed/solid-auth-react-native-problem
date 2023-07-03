ad793feef3355a3df5a03a7a6ee9dfa90039eb02
jaxoncreed <jaxoncreed@gmail.com>
2023-07-02 21:49:51 -0400

    Prove the problem extends to the underlying solid-client-autn-browser

--- app/_layout.js
+++ app/_layout.js
@@ -1,12 +1,9 @@
-import { SolidAuthProvider, useSolidAuth } from "ldo-solid-react";
 import { Stack } from "expo-router";
 
 export default function App() {
   return (
-   <SolidAuthProvider>
     <Stack
       screenOptions={{ header: () => undefined, title: "Test App" }}
     />
-   </SolidAuthProvider>
   );
 }
--- app/index.js
+++ app/index.js
@@ -1,19 +1,29 @@
 import { Button, Text, View } from 'react-native';
-import { useSolidAuth } from "ldo-solid-react";
+import { login, getDefaultSession, handleIncomingRedirect } from "@inrupt/solid-client-authn-browser";
+import { useEffect, useState } from 'react';
 
-export default function SubComponent() {
-  const { login, session } = useSolidAuth();
+export default function App() {
+  const [session, setSession] = useState({});
+  useEffect(() => {
+    setSession({ ...getDefaultSession().info });
+    handleIncomingRedirect().then(() => {
+      console.log(getDefaultSession().info);
+      setSession({ ...getDefaultSession().info });
+    });
+  }, []);
 
   return (
     <View>
-      <Text>Look in the console for more information</Text>
       <Text>Is Logged In: {session.isLoggedIn ? "true" : "false"}</Text>
       <Text>WebId: {session.webId}</Text>
       <Button
-        onPress={() => login("https://solidweb.me/")}
+        onPress={() => login({
+          oidcIssuer: "https://solidweb.me/",
+          redirectUrl: window.location.href,
+          clientName: "SomeClient",
+        })}
         title="Log In"
       />
     </View>
   );
 }
-
