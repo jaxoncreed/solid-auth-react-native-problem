8779ffa1848bc6f0611f10f8eaa426f4c7c7d733
jaxoncreed <jaxoncreed@gmail.com>
2023-07-02 21:22:02 -0400

    Works with ldo-solid-react

--- App.js
+++ App.js
@@ -1,30 +1,29 @@
 import { Button, StyleSheet, Text, View } from 'react-native';
 import { login, getDefaultSession, handleIncomingRedirect } from "solid-authn-react-native";
 import { useEffect, useState } from 'react';
+import { SolidAuthProvider, useSolidAuth } from "ldo-solid-react";
 
-export default function App() {
-  const [session, setSession] = useState({});
-  useEffect(() => {
-    setSession({ ...getDefaultSession().info });
-    handleIncomingRedirect().then(() => {
-      console.log(getDefaultSession().info);
-      setSession({ ...getDefaultSession().info });
-    });
-  }, []);
+function SubComponent() {
+  const { login, session } = useSolidAuth();
 
   return (
     <View>
       <Text>Look in the console for more information</Text>
-      <Text>Is Logged In: {session.isLoggedIn}</Text>
+      <Text>Is Logged In: {session.isLoggedIn ? "true" : "false"}</Text>
       <Text>WebId: {session.webId}</Text>
       <Button
-        onPress={() => login({
-          oidcIssuer: "https://solidweb.me/",
-          redirectUrl: window.location.href,
-          clientName: "SomeClient",
-        })}
+        onPress={() => login("https://solidweb.me/")}
         title="Log In"
       />
     </View>
+  );
+}
+
+export default function App() {
+
+  return (
+   <SolidAuthProvider>
+    <SubComponent />
+   </SolidAuthProvider>
   );
 }
